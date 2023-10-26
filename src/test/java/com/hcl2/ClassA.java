6f1f968c252259f313bc3b9ce423dd67e32c2f45
Rajaduraiveerathamilan <saibabarajaa@gmail.com>
2023-10-26 12:37:56 +0530

    Red Code

--- src/test/java/com/hcl2/ClassA.java
+++ src/test/java/com/hcl2/ClassA.java
@@ -17,17 +17,16 @@ private void method3() {
 
 }
 
-private void method4() {
-	 System.out.println("4");
-	
-	
-}
-private void method5() {
-	 System.out.println("5");
+private void method7() {
+	System.out.println("7");
 
 }
-private void method6() {
-	 System.out.println("6");
+private void method8() {
+	System.out.println("8");
+
+}
+private void method9() {
+	System.out.println("9");
 
 }
 		
